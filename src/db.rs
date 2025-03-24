9adb6b141737f9151ca815dfc509f5a3e7820150
corentin-roussel <corentin.roussel@laplateforme.io>
2025-03-24 19:30:07 +0100

    change previous connection to db to match postgres

--- src/db.rs
+++ src/db.rs
@@ -1,15 +1,34 @@
-use anyhow::Ok;
+use anyhow::Result;
 use async_std::net::TcpStream;
-use once_cell::sync::Lazy;
-use std::env;
-use tiberius::{Client, Config};
+use tokio::spawn;
+use tokio_postgres::{Client, Error, NoTls};
 
-static CONN_STR_PORT: Lazy<string> = Lazy::new(|| {
-    env::var("TIBERIUS_TEST_CONNECTION_STRING").unwrap_or_else(
-        |_| "server=tcp:127.0.0.1\\postgres,5432;database=todo_list;user=corentin;password=test",
-    )
-});
+async fn main() -> Result<(), Error> {
+    let conn_str = "host=127.0.0.1 user=corentin password=test dbname=todo_list";
+
+    let (client, connection) = tokio_postgres::connect(conn_str, NoTls).await?;
+
+    spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("Connection error: {}", e);
+        }
+    });
+
+    println!("Connected to PostgreSQL!");
+    Ok(())
+}
 
 async fn connect_through_port() -> anyhow::Result<()> {
     let config = Config::from_ado_string(&CONN_STR_PORT)?;
+
+    let tcp = TcpStream::connect(config.get_addr()).await?;
+
+    tcp.set_nodelay(true)?;
+
+    let client = Client::connect(config, tcp).await?;
+    println!("Successfully connected to server.");
+
+    client.close().await?;
+
+    Ok(())
 }
